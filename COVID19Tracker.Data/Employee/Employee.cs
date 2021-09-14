b7f6be41e4784917040cc17267ed788b2f0008a9
Nicholas Stoner <nicholas.c.stoner@gmail.com>
2021-09-14 18:11:39 -0400

    added employee data class

---
+++ COVID19Tracker.Data/Employee/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COVID19Tracker.Data.Employee
+{
+    public class Employee
+    {
+        [Key]
+        public int ID { get; set; }
+        
+        [Required]
+        public int BadgeId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [ForeignKey(nameof(Department))]
+        public int DepartmentId { get; set; }
+        public virtual Department Department { get; set; }
+
+        [ForeignKey(nameof(HealthStatus)]
+        public int healthStatusId { get; set; }
+        public virtual HealthStatus HealthStatus { get; set; }
+
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset? ModifiedDate { get; set; }
+
+
+
+    }
+}
