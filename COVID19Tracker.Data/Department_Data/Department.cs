1b85ece9460ffb04dbc2c5c4f03ed88c7054cb06
Marcus <marcus317@gmail.com>
2021-09-14 20:12:41 -0400

    Updated methods for Department

--- COVID19Tracker.Data/Department_Data/Department.cs
+++ COVID19Tracker.Data/Department_Data/Department.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace COVID19Tracker.Data.Department_Data
 {
     public class Department
     {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        public string DepartmentLocation { get; set; }
+
+        public DateTime LastCleanDate { get; set; }
+
+        public int WeeklyTests { get; set; }
     }
 }
--- COVID19Tracker.Models/Department_Models/DepartmentCreate.cs
+++ COVID19Tracker.Models/Department_Models/DepartmentCreate.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace COVID19Tracker.Models.DepartmentModel
 {
-    class DepartmentCreate
+    public class DepartmentCreate
     {
+        [Required]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        public string DepartmentName { get; set; }
     }
 }
