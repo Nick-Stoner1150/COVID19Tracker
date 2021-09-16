697397e9a61a7ed7a3a680a09f6c343a87cf5032
Denzel <dbledsoe374@gmail.com>
2021-09-16 15:07:27 -0400

    Updated HealthStatus Data class.

--- COVID19Tracker.Data/HealthStatus_Data/HealthStatus.cs
+++ COVID19Tracker.Data/HealthStatus_Data/HealthStatus.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,23 @@ namespace COVID19Tracker.Data.HealthStatus_Data
 {
     public class HealthStatus
     {
+        [Key]
         public int HealthStatusId { get; set; }
+
+        [Required]
         public bool Vaccinated { get; set; }
+
+        [Required]
         public bool HasCovid { get; set; }
+
+        [Required]
         public bool Hospitalized { get; set; }
+
+        [Required]
         public string Comorbidities { get; set; }
+
         public DateTime QuarantinedDate { get; set; }
+
         public DateTime LastTestedDate { get; set; }
     }
 }
