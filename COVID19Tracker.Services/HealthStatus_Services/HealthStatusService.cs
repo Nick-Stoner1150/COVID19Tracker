1ef3846d47773d99ad4637f10e5e7353275df710
Denzel <dbledsoe374@gmail.com>
2021-09-16 14:35:55 -0400

    Added HealthStatus Service and Controller.

---
+++ COVID19Tracker.Services/HealthStatus_Services/HealthStatusService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COVID19Tracker.Services.HealthStatus_Services
+{
+    public class HealthStatusService
+    {
+
+    }
+}
---
+++ COVID19Tracker.WebApi/Controllers/HealthStatus_Controllers/HealthStatusController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace COVID19Tracker.WebApi.Controllers.HealthStatus_Controllers
+{
+    public class HealthStatusController : ApiController
+    {
+
+    }
+}
