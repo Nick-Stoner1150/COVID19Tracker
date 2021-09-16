42ab10744da783b178c8fb885c4514f04f15e18d
Denzel <dbledsoe374@gmail.com>
2021-09-16 15:32:31 -0400

    HealthStatus Controller coded.

--- COVID19Tracker.WebApi/Controllers/HealthStatus_Controllers/HealthStatusController.cs
+++ COVID19Tracker.WebApi/Controllers/HealthStatus_Controllers/HealthStatusController.cs
@@ -1,4 +1,5 @@
 ﻿using COVID19Tracker.Models.HealthStatus_Models;
+using COVID19Tracker.Services.HealthStatus_Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,12 @@ namespace COVID19Tracker.WebApi.Controllers.HealthStatus_Controllers
 {
     public class HealthStatusController : ApiController
     {
+        private HealthStatusService HealthStatusService()
+        {
+            var svc = new HealthStatusService();
+            return svc;
+        }
+
         [HttpPost]
         public async Task<IHttpActionResult> Post (HealthStatusCreate healthStatus)
         {
@@ -19,7 +26,34 @@ namespace COVID19Tracker.WebApi.Controllers.HealthStatus_Controllers
                 return BadRequest(ModelState);
             }
 
+            var svc = HealthStatusService();
+            var success = await svc.Post(healthStatus);
 
+            if (success)
+            {
+                return Ok("Employee health status has successfully been submitted to the database.");
+            }
+            return InternalServerError();
+        }
+
+        [HttpPut]
+        public async Task<IHttpActionResult> Put(HealthStatusEdit healthStatus, int id)
+        {
+            if (id < 1 || id != healthStatus.HealthStatusId || healthStatus is null)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var svc = HealthStatusService();
+            var success = await svc.Put(healthStatus, id);
+            if (success)
+            {
+                return Ok("Updated information submitted to the database.");
+            }
+            return InternalServerError();
         }
     }
 }
