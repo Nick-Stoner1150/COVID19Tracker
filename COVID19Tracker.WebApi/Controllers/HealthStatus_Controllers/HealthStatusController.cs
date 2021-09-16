872327dbcb9015dd94b3d7ec9b7deba928cf0bc3
Denzel <dbledsoe374@gmail.com>
2021-09-16 15:14:44 -0400

    Working on HealthStatusController.

--- COVID19Tracker.WebApi/Controllers/HealthStatus_Controllers/HealthStatusController.cs
+++ COVID19Tracker.WebApi/Controllers/HealthStatus_Controllers/HealthStatusController.cs
@@ -1,14 +1,25 @@
-﻿using System;
+﻿using COVID19Tracker.Models.HealthStatus_Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace COVID19Tracker.WebApi.Controllers.HealthStatus_Controllers
 {
     public class HealthStatusController : ApiController
     {
+        [HttpPost]
+        public async Task<IHttpActionResult> Post (HealthStatusCreate healthStatus)
+        {
+            if (healthStatus is null || !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
+
+        }
     }
 }
