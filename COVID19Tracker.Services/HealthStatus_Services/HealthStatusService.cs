0298a6091634d36f37a2ec94a848bfb56274ee17
Denzel <dbledsoe374@gmail.com>
2021-09-16 15:04:38 -0400

    Coded Methods into HealthStatus Services.

--- COVID19Tracker.Services/HealthStatus_Services/HealthStatusService.cs
+++ COVID19Tracker.Services/HealthStatus_Services/HealthStatusService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using COVID19Tracker.Data;
+using COVID19Tracker.Data.HealthStatus_Data;
+using COVID19Tracker.Models.HealthStatus_Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,45 @@ namespace COVID19Tracker.Services.HealthStatus_Services
 {
     public class HealthStatusService
     {
+        public async Task<bool> Post(HealthStatusCreate healthStatus)
+        {
+            var entity = new HealthStatus
+            {
+                HealthStatusId = healthStatus.HealthStatusId,
+                Vaccinated = healthStatus.Vaccinated,
+                HasCovid = healthStatus.HasCovid,
+                Hospitalized = healthStatus.Hospitalized,
+                Comorbidities = healthStatus.Comorbidities,
+                QuarantinedDate = healthStatus.QuarantinedDate,
+                LastTestedDate = healthStatus.LastTestedDate
+            };
 
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.HealthStatuses.Add(entity);
+                return await ctx.SaveChangesAsync() > 0;
+            }
+        }
+        public async Task<bool> Put(HealthStatusEdit healthStatus, int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var oldHealth = await ctx.HealthStatuses.FindAsync(id);
+                if (oldHealth is null)
+                {
+                    return false;
+                }
+
+                oldHealth.HealthStatusId = healthStatus.HealthStatusId;
+                oldHealth.Vaccinated = healthStatus.Vaccinated;
+                oldHealth.HasCovid = healthStatus.HasCovid;
+                oldHealth.Hospitalized = healthStatus.Hospitalized;
+                oldHealth.Comorbidities = healthStatus.Comorbidities;
+                oldHealth.QuarantinedDate = healthStatus.QuarantinedDate;
+                oldHealth.LastTestedDate = healthStatus.LastTestedDate;
+
+                return await ctx.SaveChangesAsync() > 0;
+            }
+        }
     }
 }
