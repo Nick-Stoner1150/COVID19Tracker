e95f1799cf950a696b88770f69bf003a52f8c1d6
Marcus <marcus317@gmail.com>
2021-09-16 20:56:28 -0400

    Added get methods

--- COVID19Tracker.WebApi/Controllers/DepartmentController.cs
+++ COVID19Tracker.WebApi/Controllers/DepartmentController.cs
@@ -1,4 +1,5 @@
-﻿using COVID19Tracker.Models.DepartmentModel;
+﻿using COVID19Tracker.Models.Department_Models;
+using COVID19Tracker.Models.DepartmentModel;
 using COVID19Tracker.Services.DepartmentServices;
 using Microsoft.AspNet.Identity;
 using System;
@@ -15,7 +16,7 @@ namespace COVID19Tracker.WebApi.Controllers
     {
         private DepartmentService CreateDepartmentService()
         {
-
+            
             var departmentService = new DepartmentService();
             return departmentService;
         }
@@ -29,34 +30,51 @@ namespace COVID19Tracker.WebApi.Controllers
                 return BadRequest(ModelState);
             }
 
-            var svc = CreateDepartmentService();
-            var success = await svc.Post(department);
+            var entity = CreateDepartmentService();
+            var success = await entity.Post(department);
             if (success)
             {
                 return Ok();
             }
             return InternalServerError();
+
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> Get()
+        {
+            var entity = CreateDepartmentService();
+            var depts = await entity.Get();
+            return Ok(depts);
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> GetDeptEmploy()
+        {
+            var entity = CreateDepartmentService();
+            var deptsEmploy = await entity.GetDeptEmploy();
+            return Ok(deptsEmploy);
         }
 
 
 
 
 
+
         [HttpPut]
-        public async Task<IHttpActionResult> Put([FromBody] DepartmentEdit department, [FromUri] string name)
+        public async Task<IHttpActionResult>Put([FromBody] DepartmentEdit department, [FromUri]string name)
         {
+            
             if (!ModelState.IsValid)
+            {
                 return BadRequest(ModelState);
-
-            var service = CreateDepartmentService();
-            var success = await service.Put(department, name);
-
+            }
+            var entity = CreateDepartmentService();
+            var success = await entity.Put(department, name);
             if (success)
             {
                 return Ok();
             }
-                
-
             return InternalServerError();
         }
 
